289e0027014f37d733470d1b93acca10c747c5f5
Amilton Jose <amiltonjosefn2019@gmail.com>
2023-03-02 01:13:40 -0300

    Create Task model

--- src/main/java/com/amilton/todosimple/models/User.java
+++ src/main/java/com/amilton/todosimple/models/User.java
@@ -1,13 +1,16 @@
 package com.amilton.todosimple.models;
 
-import java.util.List;
+
 import java.util.Objects;
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -44,7 +47,8 @@ public class User {
     @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
 
     public User() {
@@ -82,6 +86,14 @@ public class User {
         this.password = password;
     }
 
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 
 
     @Override
