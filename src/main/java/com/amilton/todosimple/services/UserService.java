087eb30fca9aed1c1ce1e6917936a2e3bea6dd61
Amilton Jose <amiltonjosefn2019@gmail.com>
2023-03-02 02:36:04 -0300

    Refactor UserService

--- src/main/java/com/amilton/todosimple/services/UserService.java
+++ src/main/java/com/amilton/todosimple/services/UserService.java
@@ -2,13 +2,11 @@ package com.amilton.todosimple.services;
 
 import java.util.Optional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.amilton.todosimple.models.User;
-import com.amilton.todosimple.repositories.TaskRepository;
 import com.amilton.todosimple.repositories.UserRepository;
 
 @Service
@@ -16,9 +14,6 @@ public class UserService {
     
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private TaskRepository taskRepository;
 
     public User findById(Long id){
        Optional<User> user = this.userRepository.findById(id);
@@ -31,7 +26,6 @@ public class UserService {
     public User create(User obj){
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
@@ -45,7 +39,7 @@ public class UserService {
     public void delete(Long id){
         findById(id);
         try{
-            this.taskRepository.deleteById(id);
+            this.userRepository.deleteById(id);
         }catch(Exception e){
             throw new RuntimeException("Não é possivel excluir pois há entidades relacionadas!");
         }
